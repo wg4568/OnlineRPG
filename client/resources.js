bd116231be7ea7ce2389f93ad1acd681d36bc6a9
William Gardner <wg4568@gmail.com>
2018-04-28 15:36:57 -0700

    added sprite loader

---
+++ client/resources.js
@@ -0,0 +1,12 @@
+var Sprites = {
+	grass: new Sprite.Image("sprites/grass.png"),
+	dirt: new Sprite.Image("sprites/dirt.png"),
+	dirt_corner_tl: new Sprite.Image("sprites/dirtcornerTL.png"),
+	dirt_corner_tr: new Sprite.Image("sprites/dirtcornerTR.png"),
+	dirt_corner_bl: new Sprite.Image("sprites/dirtcornerBL.png"),
+	dirt_corner_br: new Sprite.Image("sprites/dirtcornerBR.png"),
+	dirt_border_t: new Sprite.Image("sprites/dirtborderT.png"),
+	dirt_border_b: new Sprite.Image("sprites/dirtborderB.png"),
+	dirt_border_l: new Sprite.Image("sprites/dirtborderL.png"),
+	dirt_border_r: new Sprite.Image("sprites/dirtborderR.png")
+}
